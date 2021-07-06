0ed633c591b3d6472710caf8315948386caae819
E202033B <mathias.nocet@etu.univ-nantes.fr>
2021-07-06 17:27:08 +0200

    TRC page optimisé

--- public/js/global/generate_menu.js
+++ public/js/global/generate_menu.js
@@ -5,7 +5,7 @@ window.onload=function(){
       li.className="menu_item_img";
       li.id="menu-6-item";
       li.innerHTML=  "<a class='right_item_menu' href='/account/' title='personal page'>"
-                    +"<img src='/img/home.svg' height='24' width='24'/>"
+                    +"<img src='/img/home.svg' height='48' width='48'/>"
                     +"</a>"
 
       document.getElementById("top_menu").appendChild(li);
@@ -15,7 +15,7 @@ window.onload=function(){
       li.className="menu_item_img";
       li.id="menu-6-item";
       li.innerHTML=  "<a class='right_item_menu' href='/login/' title='personal page'>"
-                    +"<img src='/img/login.svg' height='24' width='24'/>"
+                    +"<img src='/img/login.svg' height='48' width='48'/>"
                     +"</a>"
 
       document.getElementById("top_menu").appendChild(li);
