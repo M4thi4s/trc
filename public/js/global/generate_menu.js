2923cf03fbb6d538287a8d667f30e0a556a3c812
E202033B <mathias.nocet@etu.univ-nantes.fr>
2021-07-09 15:57:40 +0200

    mise à jours menu

--- public/js/global/generate_menu.js
+++ public/js/global/generate_menu.js
@@ -1,26 +1,44 @@
 window.onload=function(){
   checkAuthentification(
     function(res){
-      var li=document.createElement("li");
-      li.className="menu_item_img";
-      li.id="menu-6-item";
-      li.innerHTML=  "<a class='right_item_menu' href='/account/' title='personal page'>"
-                    +"<img src='/img/home.svg' height='48' width='48'/>"
-                    +"</a>"
+      var img=document.createElement("img");
+      img.src="/img/home.svg";
+      img.height="40";
+      img.width="40";
+      img.className="menu_item_img";
 
-      document.getElementById("top_menu").appendChild(li);
+      var container=document.createElement("a");
+      container.href="/account/";
+
+      container.appendChild(img);
+      document.getElementById("topMenuTableRight").appendChild(container);
+
+      
+      var img2=document.createElement("img");
+      img2.src="/img/disconnect.svg";
+      img2.height="40";
+      img2.width="40";
+      img2.className="menu_item_img";
+      img2.onclick = function(){
+        localStorage.clear(); document.location.reload();
+      };
+
+      document.getElementById("topMenuTableRight").appendChild(img2);
+
     },
     function(err){
-      var li=document.createElement("li");
-      li.className="menu_item_img";
-      li.id="menu-6-item";
-      li.innerHTML=  "<a class='right_item_menu' href='/login/' title='personal page'>"
-                    +"<img src='/img/login.svg' height='48' width='48'/>"
-                    +"</a>"
+      var img=document.createElement("img");
+      img.src="/img/login.svg";
+      img.height="40";
+      img.width="40";
+      img.className="menu_item_img";
 
-      document.getElementById("top_menu").appendChild(li);
+      var container=document.createElement("a");
+      container.href="/login/";
+
+      container.appendChild(img);
+      document.getElementById("topMenuTableRight").appendChild(container);
 
     }
-  )
-
+  );
 }
